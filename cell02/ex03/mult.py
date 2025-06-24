341691a6c6eb4c096caedfaf9ac17976d270f47e
Panuwit-KPJ <panuwit.kpj@gmail.com>
2025-06-24 08:52:37 +0000

    hw

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,10 +1,13 @@
-print("Enter the first number:")
-num1=input()
-
-print("Enter the second number:")
-num2=input()
-
-num1=int(num1)
-num2=int(num2)
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
 
 result=num1 * num2
+
+print(f"{num1}*{num2}={result}")
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print(" The result is positive and negative.")

29bcee92c5d2bae031725f267e94fba6d1c80722
Panuwit-KPJ <panuwit.kpj@gmail.com>
2025-06-24 08:19:20 +0000

    hw

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+num=input()
+num = int(num)
+
+if num > 0:
+    print("This number is positive.")
+elif num < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
